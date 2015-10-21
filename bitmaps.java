76b587e6b9c11f1d71d833edf59bccc655fe89bf
kingsman142 <kingsman142@yahoo.com>
2015-10-21 19:22:13 -0400

    updated bitmaps.java

--- bitmaps.java
+++ bitmaps.java
@@ -3,6 +3,13 @@ import java.util.*;
 import java.awt.*;
 
 public class bitmaps{
+	public void paintCOmponent(Graphics g){
+		super.paintComponent(g);
+
+		g.setColor(Color.BLACK);
+		g.drawLine(1, 2, 1, 2);
+	}
+
 	public static void main(String[] args) throws IOException, NoSuchElementException {
 		File bitmapFile = new File("bitmap.bmp");
 		Scanner reader = new Scanner(bitmapFile);
@@ -27,5 +34,13 @@ public class bitmaps{
 		System.out.println(onesZeros);
 
 		reader.close();
+
+		JFrame window = new JFrame();
+		Panel p = new Panel();
+		window.setTitle("bitmaps");
+		window.setSize(500, 500);
+		window.setVisible(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.add(p);
 	}
 }
