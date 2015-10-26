7ef54207835b494ede72f1bda3646a780e3bc365
kingsman142 <kingsman142@yahoo.com>
2015-10-25 22:51:33 -0400

    basically finished bitmaps.java and sample bitmap file

--- bitmaps.java
+++ bitmaps.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import javax.imageio.*;
 import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -8,66 +9,71 @@ import javax.swing.*;
 public class bitmaps extends JApplet{
 	public void init(int[][] numArray){
 		getContentPane().setBackground(Color.red);
-		getBits();
 	}
 
-	// static int[][] numArray = new int[][] {
-	// 	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{1, 1, 0, 1, 0, 0, 0, 0, 0, 0},
-	// 	{1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
-	// 	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{1, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-	// 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	// 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0} };
+	public static void convertImage(File image){
+		try{
+			BufferedImage img = ImageIO.read(image);
+			PrintWriter writer = new PrintWriter("C:/Users/kingsman142/Desktop/Projects/bitmap.bmp");
+			int width = img.getWidth();
+			int height = img.getHeight();
+			int[] bitLine = new int[width];
+			System.out.println("Width: " + width);
+			System.out.println("Height: " + height);
+
+			for(int row = 0; row < height; row++){
+				for(int column = 0; column < width; column++){
+					Color color = new Color(img.getRGB(column, row));
+					double r = color.getRed();
+					double g = color.getGreen();
+					double b = color.getBlue();
+					double result = .3*r + .59*g + .11*b;
+					//System.out.println("Result: " + result + ", " + r + ", " + g + ", " + b);
+					int bOrW = 0;
+					if(result < 127.5 && result >= 0) {
+						bOrW = 0;
+					} else if(result >= 127.5 && result <= 255){
+						bOrW = 1;
+					} else{
+						bOrW = 2;
+					}
+					bitLine[column] = bOrW;
+				}
+				String line = "";
+				for(int i: bitLine){
+					line += Integer.toString(i);
+				}
+				System.out.println(line);
+
+				writer.println(line);
+			}
+
+			writer.close();
+		} catch(Exception e){
+			e.printStackTrace();
+		} finally{
+		}
+	}
 
 	//This function reads from a bitmap file and stores the characters (0s and 1s) into arrayLists
 	public static int[][] getBits(){
-		File bitmap;
-		Scanner reader;
-		int[][] numArray = new int[20][10];
-		char[][] charArray = new char[20][10];
+		int[][] numArray = new int[200][100];
 
 		try{
-			bitmap = new File("C:/Users/kingsman142/Desktop/Projects/bitmap.bmp");
-			reader = new Scanner(bitmap);
-
+			File bitmap = new File("C:/Users/kingsman142/Desktop/Projects/bitmap.bmp");
+			Scanner reader = new Scanner(bitmap);
 			int row = 0;
 			int column = 0;
-
 			String readStrings = "";
-			System.out.println("1");
 
 			while(reader.hasNextLine()){
-				System.out.println("2");
 				readStrings = reader.nextLine();
 
 				for(column = 0; column < readStrings.toCharArray().length; column++){
-					System.out.println("3");
-					charArray[row][column] = readStrings.toCharArray()[column];
 					numArray[row][column] = Character.getNumericValue(readStrings.toCharArray()[column]);
-					System.out.println("Success: " + charArray[row][column] + " at [ " + column + ", " + row + " ]");
-					System.out.println("4");
 				}
 
-				for(column = column; column < 10; column++){
-					numArray[row][column] = 0;
-					charArray[row][column] = '0';
-				}
 				row++;
-				System.out.println("5");
 			}
 
 			reader.close();
@@ -82,50 +88,28 @@ public class bitmaps extends JApplet{
 		super.paint(g);
 		g.setColor(Color.black);
 
-		//int[][] numArray = new int[20][10];
 		int[][] numArray = getBits();
 
-		// if(Arrays.equals(charArray, getBits())){
-		// 	g.drawString("2", 125, 200);
-		// 	numArray = getBits();
-		// } else if(numArray == null){
-		// 	g.drawString("4", 125, 200);
-		// } else{
-		// 	g.drawString("3", 125, 200);
-		// 	numArray = getBits();
-		// }
 		int row = 0;
 		int column = 0;
-		int[][] ex = {{1, 2, 3}, {4, 5, 6}};
 
-		for(row = 0; row < 20; row++){
-			for(column = 0; column < 10; column++){
-				g.drawString("1", 200, 50);
-				g.drawString(Integer.toString(row), 150, 10 + row*10);
-				g.drawString(Integer.toString(column), 125, 10 + column*10);
-				g.drawString(Integer.toString(numArray[row][column]), column*10, 210 + row*10);
+		for(row = 0; row < numArray.length; row++){
+			for(column = 0; column < numArray[row].length; column++){
 				if(numArray[row][column] == 1){
-					g.setColor(Color.blue);
+					g.setColor(Color.white);
 				} else if(numArray[row][column] == 0){
 					g.setColor(Color.black);
 				} else{
 					g.setColor(Color.yellow);
 				}
 
-				g.fillRect(column*10, row*10, 10, 10);
+				g.fillRect(column, row, 1, 1);
 			}
 		}
 	}
 
 	public static void main(String[] args){
-		int[][] numArray = getBits();
-		//char[][] charArray = getBits();
-		for(int row = 0; row < 20; row++){
-			for(int column = 0; column < 10; column++){
-				System.out.print(String.valueOf(numArray[row][column]) + " ");
-				//System.out.print(charArray[row][column] + " ");
-			}
-			System.out.println("");
-		}
+		//convertImage(new File("C:/Users/kingsman142/Desktop/Projects/face.png"));
+		convertImage(new File("http://www.absyx.fr/sites/default/files/imagecache/Preset-4-3/imagefield/test-page/image-focus.jpg"));
 	}
 }
