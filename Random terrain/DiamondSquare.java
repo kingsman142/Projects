2dafcfa45f7c8deb07b034a33d09053633cd0726
kingsman142 <kingsman142@yahoo.com>
2015-12-19 21:51:49 -0500

    finished DiamondSquare program

--- Random terrain/DiamondSquare.java
+++ Random terrain/DiamondSquare.java
@@ -1,10 +1,40 @@
 import java.util.*;
+import java.awt.*;
+import javax.swing.*;
 
-public class DiamondSquare{
-	public static final int LENGTH = 4;
+public class DiamondSquare extends JFrame{
+	public static final int HEIGHT = 500;
+	public static final int WIDTH = 500;
+	public static final int LENGTH = 8;
 	public static final int MAX_SQUARES = (int) Math.pow(2, LENGTH*2);
 	public static Random randomNum = new Random();
 	public static double[][] heightmap = new double[(int) Math.pow(2, LENGTH)+1][(int) Math.pow(2, LENGTH)+1];
+
+	public DiamondSquare(){
+		setTitle("Diamond Square Bitmap");
+		setSize(WIDTH, HEIGHT);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+	public void paint(Graphics g){
+		for(int i = 0; i < heightmap.length; i++){
+			for(int j = 0; j < heightmap.length; j++){
+				float value = (float) heightmap[i][j];
+				//System.out.println(value);
+				boolean grey = true;
+				if(grey){
+					g.setColor(new Color(value, value, value));
+				}
+				else{
+					if(heightmap[i][j] >= 0 && heightmap[i][j] < .33) g.setColor(new Color(44, 176, 55));
+					else if(heightmap[i][j] >= .33 && heightmap[i][j] < .66) g.setColor(new Color(133, 159, 168));
+					else g.setColor(new Color(255, 255, 255));
+				}
+				g.drawLine(i, j, i, j);
+			}
+		}
+	}
 
 	public static void printHeightmap(){
 		for(int i = 0; i < heightmap.length; i++){
@@ -46,15 +76,12 @@ public class DiamondSquare{
 		double bottomLeftValue = 0;
 		double bottomRightValue = 0;
 
-		//System.out.println("Center: x: " + center[0] + " , y: " + center[1]);
 		if(center[1] != 0 && center[0] != 0) topLeftValue = heightmap[center[1] - height/2][center[0] - width/2];
 		if(center[1] != 0 && center[0] != heightmap.length) topRightValue = heightmap[center[1] + height/2][center[0] - width/2];
-		//System.out.println("BottomRight: " + (center[0] + width/2) + " and " + (center[1] - height/2) + " , height: " + height + " , width: " + width);
 		if(center[1] != heightmap.length && center[0] != 0) bottomLeftValue = heightmap[center[1] - height/2][center[0] + width/2];
-		//System.out.println("BottomRight: " + (center[0] + width/2) + " and " + (center[1] + height/2) + " , height: " + height + " , width: " + width);
 		if(center[1] != heightmap.length && center[0] != heightmap.length) bottomRightValue = heightmap[center[1] + height/2][center[0] + width/2];
 
-		if(heightmap[center[1]][center[0]] == 0) heightmap[center[1]][center[0]] = averageValue(topLeftValue, topRightValue, bottomLeftValue, bottomRightValue) + plusMinus()*(randomNum.nextDouble()/Math.pow(2, numIteration));
+		if(heightmap[center[1]][center[0]] == 0) heightmap[center[1]][center[0]] = averageValue(topLeftValue, topRightValue, bottomLeftValue, bottomRightValue) + plusMinus()*(randomNum.nextDouble()/Math.pow(2, numIteration/2));
 		if(heightmap[center[1]][center[0]] < 0) heightmap[center[1]][center[0]] = 0;
 		else if(heightmap[center[1]][center[0]] > 1) heightmap[center[1]][center[0]] = 1;
 	}
@@ -67,7 +94,7 @@ public class DiamondSquare{
 
 		if(heightmap[1][left] == 0){
 			if(left == 0) heightmap[center[1]][left] = averageValue(0, heightmap[center[1] + height/2][left], heightmap[center[1] - height/2][left], heightmap[center[1]][center[0]]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration);
-			else heightmap[center[1]][left] = averageValue(heightmap[center[1]][left - width/2], heightmap[center[1] + height/2][left], heightmap[center[1] - height/2][left], heightmap[center[1]][center[0]]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration);
+			else heightmap[center[1]][left] = averageValue(heightmap[center[1]][left - width/2], heightmap[center[1] + height/2][left], heightmap[center[1] - height/2][left], heightmap[center[1]][center[0]]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration/2);
 
 			if(heightmap[center[1]][left] < 0) heightmap[center[1]][left] = 0;
 			else if(heightmap[center[1]][left] > 1) heightmap[center[1]][left] = 1;
@@ -75,7 +102,7 @@ public class DiamondSquare{
 
 		if(heightmap[top][center[0]] == 0){
 			if(top == 0) heightmap[top][center[0]] = averageValue(0, heightmap[center[1]][center[0]], heightmap[top][center[0] - width/2], heightmap[top][center[0] + width/2]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration);
-			else heightmap[top][center[0]] = averageValue(heightmap[top - height/2][center[0]], heightmap[center[1]][center[0]], heightmap[top][center[0] - width/2], heightmap[top][center[0] + width/2]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration);
+			else heightmap[top][center[0]] = averageValue(heightmap[top - height/2][center[0]], heightmap[center[1]][center[0]], heightmap[top][center[0] - width/2], heightmap[top][center[0] + width/2]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration/2);
 
 			if(heightmap[top][center[0]] < 0) heightmap[top][center[0]] = 0;
 			else if(heightmap[top][center[0]] > 1) heightmap[top][center[0]] = 1;
@@ -83,7 +110,7 @@ public class DiamondSquare{
 
 		if(heightmap[center[1]][right] == 0){
 			if(right == heightmap.length-1) heightmap[center[1]][right] = averageValue(0, heightmap[center[1] + height/2][right], heightmap[center[1] - height/2][right], heightmap[center[1]][center[0]]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration);
-			else heightmap[center[1]][right] = averageValue(heightmap[center[1]][right + width/2], heightmap[center[1] + height/2][right], heightmap[center[1] - height/2][right], heightmap[center[1]][center[0]]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration);
+			else heightmap[center[1]][right] = averageValue(heightmap[center[1]][right + width/2], heightmap[center[1] + height/2][right], heightmap[center[1] - height/2][right], heightmap[center[1]][center[0]]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration/2);
 
 			if(heightmap[center[1]][right] < 0) heightmap[center[1]][right] = 0;
 			else if(heightmap[center[1]][right] > 1) heightmap[center[1]][right] = 1;
@@ -91,7 +118,7 @@ public class DiamondSquare{
 
 		if(heightmap[bottom][center[0]] == 0){
 			if(bottom == heightmap.length-1) heightmap[bottom][center[0]] = averageValue(0, heightmap[center[1]][center[0]], heightmap[bottom][center[0] - width/2], heightmap[bottom][center[0] + width/2]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration);
-			else heightmap[bottom][center[0]] = averageValue(heightmap[bottom - height/2][center[0]], heightmap[center[1]][center[0]], heightmap[bottom][center[0] - width/2], heightmap[bottom][center[0] + width/2]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration);
+			else heightmap[bottom][center[0]] = averageValue(heightmap[bottom - height/2][center[0]], heightmap[center[1]][center[0]], heightmap[bottom][center[0] - width/2], heightmap[bottom][center[0] + width/2]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration/2);
 
 			if(heightmap[bottom][center[0]] < 0) heightmap[bottom][center[0]] = 0;
 			else if(heightmap[bottom][center[0]] > 1) heightmap[bottom][center[0]] = 1;
@@ -101,22 +128,18 @@ public class DiamondSquare{
 	public static void transformTerrain(int[] topLeft, int[] topRight, int[] bottomLeft, int[] bottomRight, int numIteration){
 		int numSquares = (int) Math.pow(2, numIteration+2)/4;
 		if(!fullHeightmap() && numSquares <= MAX_SQUARES){
-			//int numSquares = (int) Math.pow(2, numIteration+2)/4;
 			int squaresLength = (int) Math.pow(numSquares, .5);
 			int lengthRatio = (heightmap.length-1)/squaresLength;
 			int lengthRatio2 = (heightmap.length-1)/squaresLength / 2;
 			System.out.println("NumSquares: " + numSquares + " , SquaresLength: " + squaresLength);
-			//System.out.println("Heightmap.length-1/squareslength: " + lengthRatio + " and " + lengthRatio2);
 			int width = topRight[1] - topLeft[1];
 			int height = bottomLeft[0] - topLeft[0];
 
 			for(int i = 0; i < squaresLength; i++){
 				for(int j = 0; j < squaresLength; j++){
-					int[] center = findMidpoint(lengthRatio, lengthRatio, new int[] {topLeft[0] /*+ lengthRatio2*i*/ + lengthRatio*i, topLeft[1] /*+ lengthRatio2*j*/ + lengthRatio*j});
+					int[] center = findMidpoint(lengthRatio, lengthRatio, new int[] {topLeft[0] + lengthRatio*i, topLeft[1] + lengthRatio*j});
 					displaceMidpoint(center, lengthRatio, lengthRatio, numIteration+1);
-					//System.out.printf("Center: x: %d, y: %d, value: %f\n", center[0], center[1], heightmap[center[0]][center[1]]);
 					squareStep(center, lengthRatio, lengthRatio, numIteration+1);
-					//printHeightmap();
 				}
 			}
 
@@ -138,6 +161,8 @@ public class DiamondSquare{
 
 		transformTerrain(new int[] {0, 0}, new int[] {0, heightmap.length}, new int[] {heightmap.length, 0}, new int[] {heightmap.length, heightmap.length}, 0);
 
-		printHeightmap();
+		//printHeightmap();
+
+		new DiamondSquare();
 	}
 }
