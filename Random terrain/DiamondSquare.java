efbba0652ff1b5ae70e444b3818ee37ceb5e6c27
kingsman142 <kingsman142@yahoo.com>
2015-12-20 23:36:07 -0500

    DiamondSquare now ouputs a bitmap.png file that shows the terrain

--- Random terrain/DiamondSquare.java
+++ Random terrain/DiamondSquare.java
@@ -1,13 +1,17 @@
 import java.util.*;
 import java.awt.*;
 import javax.swing.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
 
 public class DiamondSquare extends JFrame{
 	//Set constants and global variables
-	public static final int LENGTH = 10;
+	public static final int LENGTH = 11;
 	public static final int MAX_SQUARES = (int) Math.pow(4, LENGTH);
-	public static final int HEIGHT = (int) Math.pow(2, LENGTH)+100;
-	public static final int WIDTH = (int) Math.pow(2, LENGTH)+100;
+	public static final int HEIGHT = (int) Math.pow(2, LENGTH)+20;
+	public static final int WIDTH = (int) Math.pow(2, LENGTH)+20;
+	public static final int SMOOTHNESS = 0; //how consistent the heights are throughout the terrain, higher = more consistent
 	public static Random randomNum = new Random();
 	public static double[][] heightmap = new double[(int) Math.pow(2, LENGTH)+1][(int) Math.pow(2, LENGTH)+1];
 
@@ -40,6 +44,47 @@ public class DiamondSquare extends JFrame{
 				else g.setColor(new Color(255, 255, 255, 50));
 				g.drawLine(i, j, i, j);
 			}
+		}
+	}
+
+	//Outputs the heightmap/bitmap to bitmap.png
+	public static void outputBitmap(){
+		try{
+			File bitmap = new File("bitmap.png");
+			bitmap.createNewFile();
+			BufferedImage output = new BufferedImage(WIDTH-20, HEIGHT-20, BufferedImage.TYPE_INT_RGB);
+
+			for(int i = 0; i < heightmap.length-1; i++){
+				for(int j = 0; j < heightmap.length-1; j++){
+					float mapValue = (float) heightmap[i][j];
+
+					//I needed a way to combine the grey values and the colored values
+					//into the .png output file, so I just averaged both values
+					//and changed each pixel value in the image manually.
+					float rAverage;
+					float gAverage;
+					float bAverage;
+					if(mapValue >= 0 && mapValue < .33){
+						rAverage = (mapValue*255 + 44)/2;
+						gAverage = (mapValue*255 + 176)/2;
+						bAverage = (mapValue*255 + 55)/2;
+					} else if(mapValue >= .33 && mapValue < .66){
+						rAverage = (mapValue*255 + 133)/2;
+						gAverage = (mapValue*255 + 159)/2;
+						bAverage = (mapValue*255 + 168)/2;
+					} else{
+						rAverage = (mapValue*255 + 255)/2;
+						gAverage = (mapValue*255 + 255)/2;
+						bAverage = (mapValue*255 + 255)/2;
+					}
+					Color col = new Color(rAverage/255, gAverage/255, bAverage/255, mapValue/255);
+					output.setRGB(i, j, col.getRGB());
+				}
+			}
+
+			ImageIO.write(output, "png", bitmap);
+		} catch(IOException e2){
+			e2.printStackTrace();
 		}
 	}
 
@@ -84,7 +129,7 @@ public class DiamondSquare extends JFrame{
 		if(center[1] != heightmap.length && center[0] != 0) bottomLeftValue = heightmap[center[1] - height/2][center[0] + width/2];
 		if(center[1] != heightmap.length && center[0] != heightmap.length) bottomRightValue = heightmap[center[1] + height/2][center[0] + width/2];
 
-		if(heightmap[center[1]][center[0]] == 0) heightmap[center[1]][center[0]] = averageValue(topLeftValue, topRightValue, bottomLeftValue, bottomRightValue) + plusMinus()*(randomNum.nextDouble()/Math.pow(2, numIteration/2));
+		if(heightmap[center[1]][center[0]] == 0) heightmap[center[1]][center[0]] = averageValue(topLeftValue, topRightValue, bottomLeftValue, bottomRightValue) + plusMinus()*(randomNum.nextDouble()/Math.pow(2, numIteration/2+SMOOTHNESS));
 		if(heightmap[center[1]][center[0]] < 0) heightmap[center[1]][center[0]] = 0;
 		else if(heightmap[center[1]][center[0]] > 1) heightmap[center[1]][center[0]] = 1;
 	}
@@ -99,7 +144,7 @@ public class DiamondSquare extends JFrame{
 		//Displaces the point left of the center
 		if(heightmap[1][left] == 0){
 			if(left == 0) heightmap[center[1]][left] = averageValue(0, heightmap[center[1] + height/2][left], heightmap[center[1] - height/2][left], heightmap[center[1]][center[0]]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration);
-			else heightmap[center[1]][left] = averageValue(heightmap[center[1]][left - width/2], heightmap[center[1] + height/2][left], heightmap[center[1] - height/2][left], heightmap[center[1]][center[0]]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration/2);
+			else heightmap[center[1]][left] = averageValue(heightmap[center[1]][left - width/2], heightmap[center[1] + height/2][left], heightmap[center[1] - height/2][left], heightmap[center[1]][center[0]]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration/2+SMOOTHNESS);
 
 			if(heightmap[center[1]][left] < 0) heightmap[center[1]][left] = 0;
 			else if(heightmap[center[1]][left] > 1) heightmap[center[1]][left] = 1;
@@ -108,7 +153,7 @@ public class DiamondSquare extends JFrame{
 		//Displaces theh point above the center
 		if(heightmap[top][center[0]] == 0){
 			if(top == 0) heightmap[top][center[0]] = averageValue(0, heightmap[center[1]][center[0]], heightmap[top][center[0] - width/2], heightmap[top][center[0] + width/2]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration);
-			else heightmap[top][center[0]] = averageValue(heightmap[top - height/2][center[0]], heightmap[center[1]][center[0]], heightmap[top][center[0] - width/2], heightmap[top][center[0] + width/2]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration/2);
+			else heightmap[top][center[0]] = averageValue(heightmap[top - height/2][center[0]], heightmap[center[1]][center[0]], heightmap[top][center[0] - width/2], heightmap[top][center[0] + width/2]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration/2+SMOOTHNESS);
 
 			if(heightmap[top][center[0]] < 0) heightmap[top][center[0]] = 0;
 			else if(heightmap[top][center[0]] > 1) heightmap[top][center[0]] = 1;
@@ -117,7 +162,7 @@ public class DiamondSquare extends JFrame{
 		//Displaces the point right of the center
 		if(heightmap[center[1]][right] == 0){
 			if(right == heightmap.length-1) heightmap[center[1]][right] = averageValue(0, heightmap[center[1] + height/2][right], heightmap[center[1] - height/2][right], heightmap[center[1]][center[0]]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration);
-			else heightmap[center[1]][right] = averageValue(heightmap[center[1]][right + width/2], heightmap[center[1] + height/2][right], heightmap[center[1] - height/2][right], heightmap[center[1]][center[0]]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration/2);
+			else heightmap[center[1]][right] = averageValue(heightmap[center[1]][right + width/2], heightmap[center[1] + height/2][right], heightmap[center[1] - height/2][right], heightmap[center[1]][center[0]]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration/2+SMOOTHNESS);
 
 			if(heightmap[center[1]][right] < 0) heightmap[center[1]][right] = 0;
 			else if(heightmap[center[1]][right] > 1) heightmap[center[1]][right] = 1;
@@ -126,7 +171,7 @@ public class DiamondSquare extends JFrame{
 		//Displaces the point below the center
 		if(heightmap[bottom][center[0]] == 0){
 			if(bottom == heightmap.length-1) heightmap[bottom][center[0]] = averageValue(0, heightmap[center[1]][center[0]], heightmap[bottom][center[0] - width/2], heightmap[bottom][center[0] + width/2]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration);
-			else heightmap[bottom][center[0]] = averageValue(heightmap[bottom - height/2][center[0]], heightmap[center[1]][center[0]], heightmap[bottom][center[0] - width/2], heightmap[bottom][center[0] + width/2]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration/2);
+			else heightmap[bottom][center[0]] = averageValue(heightmap[bottom - height/2][center[0]], heightmap[center[1]][center[0]], heightmap[bottom][center[0] - width/2], heightmap[bottom][center[0] + width/2]) + plusMinus()*randomNum.nextDouble()/Math.pow(2, numIteration/2+SMOOTHNESS);
 
 			if(heightmap[bottom][center[0]] < 0) heightmap[bottom][center[0]] = 0;
 			else if(heightmap[bottom][center[0]] > 1) heightmap[bottom][center[0]] = 1;
@@ -160,6 +205,8 @@ public class DiamondSquare extends JFrame{
 
 		transformTerrain(new int[] {0, 0}, new int[] {0, heightmap.length}, new int[] {heightmap.length, 0}, new int[] {heightmap.length, heightmap.length}, 0);
 
-		new DiamondSquare();
+		//new DiamondSquare();
+
+		outputBitmap();
 	}
 }
