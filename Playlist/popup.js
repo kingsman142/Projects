b0dacb123c5617f165b11de5026fa6b2780b4617
kingsman142 <kingsman142@yahoo.com>
2016-03-12 15:06:47 -0500

    updated playlist app

--- Playlist/popup.js
+++ Playlist/popup.js
@@ -1,11 +1,12 @@
-var books = [];
+var books = new Array();
 
-function search_for_title(bookmarks, title, parent){
+function search_for_title(bookmarks, title, parent, booksArr){
     if(parent == null){ //First find the parent folder
         for(var i = 0; i < bookmarks.length; i++){
             if(bookmarks[i].title == title){
-                search_for_title(bookmarks[i].children, null, bookmarks[i].id);
-                console.log("BOOKS LENGTH: " + books.length);
+                search_for_title(bookmarks[i].children, null, bookmarks[i].id, booksArr);
+                console.log("BOOKS LENGTH: " + booksArr.length);
+                return booksArr;
             } else{
                 if(bookmarks[i].children){
                     var id = search_for_title(bookmarks[i].children, title);
@@ -15,9 +16,10 @@ function search_for_title(bookmarks, title, parent){
         }
     } else if(title == null){ //Parent folder is found, now just traverse the bookmarks within
         for(var i = 0; i < bookmarks.length; i++){
-            books[i] = bookmarks[i].title;
-            console.log("books[" + i + "]: " + books[i]);
+            booksArr[i].push(bookmarks[i].title);
+            //console.log("books[" + i + "]: " + this.books[i]);
         }
+        return booksArr;
     }
 
     return false;
@@ -27,13 +29,13 @@ function getBookmarks(){
     console.log("Bookmarks!");
 
     chrome.bookmarks.getTree(function(bookmarks){
-        search_for_title(bookmarks, "Music", null);
+        search_for_title(bookmarks, "Music", null, books);
     });
 
-    console.log("books: " + books.length);
-    for(var i = 0; i < books.length; i++){
-        console.log("ITEM: " + books[i]);
+    for(var i = 0; i < this.books.length; i++){
+        console.log("ITEM: " + this.books[i]);
     }
+    console.log("books: " + this.books.length);
 }
 
 getBookmarks();
