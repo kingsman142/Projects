2b250ef77c362cc99afc0ae68dcaff3ba308acde
kingsman142 <kingsman142@yahoo.com>
2016-03-12 23:04:27 -0500

    playlist commit before I mess up something big

--- Playlist/popup.js
+++ Playlist/popup.js
@@ -1,41 +1,69 @@
-var books = new Array();
+var books = [];
 
-function search_for_title(bookmarks, title, parent, booksArr){
+function getBookmarks(){
+    console.log("Bookmarks!");
+
+    books = function(){chrome.bookmarks.getTree(function(bookmarks){
+        search_for_title(bookmarks, "Music", null, books);
+    })}
+
+    books();
+
+    for(var i = 0; i < books.length; i++){
+        console.log("ITEM: " + books[i]);
+    }
+    console.log("books: " + books.length);
+}
+
+function search_for_title(bookmarks, title, parent){
     if(parent == null){ //First find the parent folder
         for(var i = 0; i < bookmarks.length; i++){
             if(bookmarks[i].title == title){
-                search_for_title(bookmarks[i].children, null, bookmarks[i].id, booksArr);
-                console.log("BOOKS LENGTH: " + booksArr.length);
-                return booksArr;
+                //console.log("MAIN --- BOOKSARR: " + booksArr.length + " , books length: " + books.length);
+                console.log("MAIN --- books length: " + books.length);
+                search_for_title(bookmarks[i].children, null, bookmarks[i].id);
+                //console.log("BOOKS LENGTH: " + booksArr.length + " , books length: " + books.length);
+                console.log("books length: " + books.length);
+                //return booksArr;
             } else{
                 if(bookmarks[i].children){
-                    var id = search_for_title(bookmarks[i].children, title);
-                    if(id) return id;
+                    //console.log("CHILDREN --- BOOKSARR: " + booksArr.length + " , books length: " + books.length);
+                    console.log("CHILDREN --- books length: " + books.length);
+                    //booksArr = [];
+                    search_for_title(bookmarks[i].children, title, parent);
+                    //this.books = [];
+                    //if(booksArr) return booksArr;
                 }
             }
         }
     } else if(title == null){ //Parent folder is found, now just traverse the bookmarks within
         for(var i = 0; i < bookmarks.length; i++){
-            booksArr[i].push(bookmarks[i].title);
-            //console.log("books[" + i + "]: " + this.books[i]);
+            console.log("booksArr[" + i + "] = " + bookmarks[i].title + " , books length: " + books.length);
+            books[i] = bookmarks[i].title;
         }
-        return booksArr;
+        //return booksArr;
     }
 
-    return false;
-}
-
-function getBookmarks(){
-    console.log("Bookmarks!");
-
-    chrome.bookmarks.getTree(function(bookmarks){
-        search_for_title(bookmarks, "Music", null, books);
-    });
-
-    for(var i = 0; i < this.books.length; i++){
-        console.log("ITEM: " + this.books[i]);
-    }
-    console.log("books: " + this.books.length);
+    //return false;
 }
 
 getBookmarks();
+
+console.log("FINAL BOOKS: " + books.length);
+
+/*var arr = [];
+
+function changArr(){
+    loopArr();
+
+    for(var i = 0; i < arr.length; i++){
+        console.log("ITEM: " + arr[i]);
+    }
+    console.log("arr: " + arr.length);
+}
+
+function loopArr(){
+    arr[1] = 999;
+}
+
+changArr();*/
