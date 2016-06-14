03c0c006f96e7535077d90369ebd5b044dc79206
kingsman142 <kingsman142@yahoo.com>
2016-06-13 22:43:26 -0400

    removed some console.logs and commented out code

--- Playlist/background.js
+++ Playlist/background.js
@@ -25,7 +25,6 @@ function recursePlaylistExec(tabs){
         chrome.tabs.executeScript(tabs[0].id, {
             code: "document.getElementsByTagName('html')[0].style.overflow = 'hidden';"
         }, function(){
-            console.log("Success");
             removeScrollbars = false;
         });
     }
@@ -36,12 +35,10 @@ function recursePlaylistExec(tabs){
             try{
                 current = results[0][0];
                 end = results[0][1];
-                console.log("current: " + current + "     ;     finish: " + end);
                 if(current == end && end != 0){
                     current = 0;
                     end = 0;
                     newURL = "https://www.youtube.com/watch?v=" + fetchRandomSong();
-                    console.log("STARTING NEW SONG: " + current + " and " + end + "     ;     new song: " + newURL);
                     chrome.tabs.update(tabs[0].id, {
                         url: newURL
                     }, function(){
@@ -53,13 +50,14 @@ function recursePlaylistExec(tabs){
                         }, 1000);
                     });
                 } else{
-                    setTimeout(function(){ console.log("Recursing"); recursePlaylistExec(tabs); }, 1000);
+                    setTimeout(function(){
+                        recursePlaylistExec(tabs);
+                    }, 1000);
                 }
             } catch(e){
                 current = 0;
                 end = 0;
                 newURL = "https://www.youtube.com/watch?v=" + fetchRandomSong();
-                console.log("STARTING NEW SONG AFTER ERROR: " + current + " and " + end + "     ;     new song: " + newURL);
                 chrome.tabs.update(tabs[0].id, {
                     url: newURL
                 }, function(){
@@ -85,11 +83,5 @@ function fetchRandomSong(){
 //Main function to run the program
 function startPlaylist(bookmarksId, tabs){
     booksID = bookmarksId;
-    setTimeout(function(){
-        document.getElementsByTagName("HTML")[0].style.overflow = "hidden";
-    }, 4000);
-    //$("html").style.overflow = "hidden";
-    //document.getElementById("html").style.overflow = "scroll";
-    //document.getElementsByTagName("HTML")[0].style.overflow = "hidden";
     recursePlaylistExec(tabs);
 }
